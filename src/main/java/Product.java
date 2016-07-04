ca31b398c63f72cb0ad29afebdee1376f37268b8
chefang <1522815630@qq.com>
2016-07-04 16:16:45 +0800

    CashRegister can get total price of multiple discount produce

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -3,14 +3,31 @@ import java.math.BigDecimal;
 public class Product {
 
 	private BigDecimal price = null;
+	private String Barcode = null;
+	private BigDecimal discount = null;
 
 	public Product(BigDecimal price) {
 		this.price = price;
 	}
 	
+	public Product(BigDecimal bigDecimal,BigDecimal discount, String barcode) {
+		this(bigDecimal);
+		this.discount = discount;
+		this.Barcode = barcode;
+	}
+
 	public BigDecimal getPrice() {
 		return price;
 	}
+
+	public String getBarcode() {
+		return Barcode;
+	}
+
+	public BigDecimal getDiscount() {
+		return discount;
+	}
+
 
 
 }
